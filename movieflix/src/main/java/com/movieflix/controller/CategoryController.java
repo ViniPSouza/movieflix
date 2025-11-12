79621c7de6cc587fa2cdc770931941a28f84586b
unknown <viniciuspereiradesou@gmail.com>
2025-11-12 06:20:28 -0300

    feat: implement Category module with controller, service, repository and migration

--- movieflix/src/main/java/com/movieflix/controller/CategoryController.java
+++ movieflix/src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,4 +1,27 @@
 package com.movieflix.controller;
 
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+
+    private CategoryService  categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategory(){
+        return categoryService.findAll();
+
+
+    }
 }
--- movieflix/src/main/java/com/movieflix/service/CategoryService.java
+++ movieflix/src/main/java/com/movieflix/service/CategoryService.java
@@ -1,4 +1,20 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
+import com.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
 }
